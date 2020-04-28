91e12beb189867b365f829d79300e293f19b83ff
Andrey Kornienko <pproger@localhost.localdomain>
2020-04-28 20:13:26 +0300

    fluorography detection of a disease using a neural network

---
+++ main.py
@@ -0,0 +1,163 @@
+#!/usr/bin/python3
+
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from tensorflow.keras.layers import BatchNormalization, Activation
+from tensorflow.python.keras.optimizers import Adam, RMSprop
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Размер изображения
+image_size = 299
+# Размер мини-выборки
+batch_size = 32
+
+
+img_path = '/home/pproger/Desktop/fluorography detection of a disease using a neural network/train_dir/Normal/CHNCXR_0163_0.png'
+img = image.load_img(img_path, target_size=(image_size, image_size))
+plt.imshow(img)
+plt.show()
+
+
+img_path = '/home/pproger/Desktop/fluorography detection of a disease using a neural network/train_dir/Tuberculosis/CHNCXR_0393_1.png'
+img = image.load_img(img_path, target_size=(image_size, image_size))
+plt.imshow(img)
+plt.show()
+
+
+# Каталог с данными для обучения
+train_dir = '/home/pproger/Desktop/fluorography detection of a disease using a neural network/train_dir'
+test_dir = '/home/pproger/Desktop/fluorography detection of a disease using a neural network/val_dir'
+# Размерность тензора на основе изображения для входных данных в нейронную сеть
+# backend Tensorflow, channels_last
+input_shape = (image_size, image_size, 3)
+
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+    rotation_range=10,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    zoom_range=0.1,
+    fill_mode='nearest')
+    
+img_path = '/home/pproger/Desktop/fluorography detection of a disease using a neural network/train_dir/Normal/CHNCXR_0001_0.png'
+img = image.load_img(img_path, target_size=(image_size, image_size))
+x = image.img_to_array(img)
+x = x.reshape((1,) + x.shape)
+i = 0
+for batch in train_datagen.flow(x, batch_size=1):
+    plt.figure(i)
+    imgplot = plt.imshow(image.array_to_img(batch[0]))
+    i += 1
+    if i % 6 == 0:
+        break
+plt.show()
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(image_size, image_size),
+    batch_size=batch_size,
+    class_mode='binary')
+    
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+test_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(image_size, image_size),
+    batch_size=batch_size,
+    class_mode='binary')
+    
+train_generator.class_indices
+
+pretrained_model = InceptionV3(weights='imagenet', include_top=False)
+
+pretrained_model.summary()
+
+pretrained_model.trainable = False
+
+x = pretrained_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(512)(x)
+x = BatchNormalization()(x)
+x = Activation('relu')(x)
+x = Dropout(0.5)(x)
+predictions = Dense(1, activation='sigmoid')(x)
+model = Model(inputs=pretrained_model.input, outputs=predictions)
+
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=1e-4), 
+              metrics=['accuracy'])
+              
+filepath="inceptionv3_best.h5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, 
+                             save_best_only=True, mode='max')
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
+                                            patience=3, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
+callbacks_list = [checkpoint, learning_rate_reduction]
+
+steps_per_epoch = 40
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=steps_per_epoch,
+    validation_data=test_generator,
+    validation_steps=5,
+    callbacks=callbacks_list,
+    epochs=20,
+    verbose=2)
+    
+model.load_weights("inceptionv3_best.h5")
+
+pretrained_model.trainable = False
+for layer in model.layers[:249]:
+   layer.trainable = False
+for layer in model.layers[249:]:
+   layer.trainable = True
+   
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=1e-4), 
+              metrics=['accuracy'])
+
+filepath="inceptionv3_fine_tuned.h5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+callbacks_list = [checkpoint, learning_rate_reduction]
+
+
+
+steps_per_epoch = 40
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=steps_per_epoch,
+    validation_data=test_generator,
+    validation_steps=5,
+    callbacks=callbacks_list,
+    epochs=5,
+    verbose=2)
+    
+model = load_model("inceptionv3_fine_tuned.h5")
+
+#img_path = 'base_dir/val_dir/Tuberculosis/CHNCXR_0337_1.png'
+img_path = '/home/pproger/Desktop/fluorography detection of a disease using a neural network/val_dir/Normal/CHNCXR_0171_0.png'
+#img_path = 'base_dir/val_dir/Tuberculosis/MCUCXR_0367_1.png'
+img = image.load_img(img_path, target_size=(image_size, image_size))
+plt.imshow(img)
+plt.show()
+
+x = image.img_to_array(img)
+x /= 255
+x = np.expand_dims(x, axis=0)
+
+prediction = model.predict(x)
+
+print(prediction)
+
+if prediction[[0]] < 0.5:
+    print('Normal')
+else:
+    print('Tuberculosis')
